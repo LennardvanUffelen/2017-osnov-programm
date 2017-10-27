74630bc0d92be9c3182dc5b823edb91de7f36556
LennardvanUffelen <lennardvanuffelen@gmail.com>
2017-10-27 17:00:13 +0200

    Add files via upload

---
+++ tokeniser.py
@@ -0,0 +1,22 @@
+import sys
+
+ssplit = sys.stdin.readlines()
+
+index = 0
+max = len(ssplit) - 1
+
+for x in range (0, max):
+	if ssplit[index].strip() == '':
+		index = index + 1
+	else:
+		print('#', ssplit[index].strip())
+		sensplit = ssplit[index].split(' ')
+		index = index + 1
+		senindex = 0
+		senmax = len(sensplit)
+		for x in range (0, senmax):
+			if sensplit[senindex].strip() == '':
+				senindex = senindex + 1
+			else:
+				print(senindex + 1, '\t', sensplit[senindex], '\t_\t_\t_\t_\t_\t_\t_\t_\t_')
+				senindex = senindex + 1
